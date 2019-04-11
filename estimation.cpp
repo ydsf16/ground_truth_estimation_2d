f7c20bccf50582ab34aad2de53211b3827e3de80
Dongsheng Yang <ydsf16@163.com>
2019-04-11 21:34:11 +0800

    Update estimation.cpp

--- estimation.cpp
+++ estimation.cpp
@@ -1,3 +1,6 @@
+// Copyright (C) 2019 Dongsheng Yang <ydsf16@buaa.edu.cn>
+//(Biologically Inspired Mobile Robot Laboratory, Robotics Institute, Beihang University)
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
